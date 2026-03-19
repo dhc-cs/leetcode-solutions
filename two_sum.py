491a46b6bad1196cbc941fb621774cade8f7d9db
Daniel Hernandez Castillo <dhernandezcastillo@csuchico.edu>
2026-03-18 23:38:57 -0700

    Optimize twoSum solution
    Refactored twoSum method to use a hash map. 
    
    Improved runtime from O(n^2) to O(n)

--- two_sum.py
+++ two_sum.py
@@ -1,8 +1,23 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n=len(nums)
 
-        for i in range(n):
-            for j in range(i+1,n):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+        #creating an empty hash map/dictionary
+        #will hold key value pairs 
+        #Each key will be the value of nums list 
+        #and each value will be its index
+        numsDict = {}
+
+        #iterating over the list to populate the hash map 
+        for i, num in enumerate(nums):
+            numsDict[num] = i
+        
+        #iterate over the list to compute the compliment
+        for i, num in enumerate(nums):
+            compliment = target - num
+
+            #check to see if compliment exits in hashmap
+            if compliment in numsDict:
+                #check to make sure we aren't using the same element twice
+                if numsDict[compliment] != i:
+                    #return the index of both numbers 
+                    return [i, numsDict[compliment]]
