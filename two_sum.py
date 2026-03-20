5e5105daab1843dc209f7e5b1634ed7a2d45e0e3
Daniel Hernandez <danihcg.23@gmail.com>
2026-03-19 23:59:02 -0700

    modified solution

--- two_sum.py
+++ two_sum.py
@@ -4,26 +4,18 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-        #creating an empty hash map/dictionary
-        #will hold key value pairs 
-        #Each key will be the value of nums list 
-        #and each value will be its index
-        numsDict = {}
+        hashTable = {}
 
-        #iterating over the list to populate the hash map 
         for i, num in enumerate(nums):
-            numsDict[num] = i
+            hashTable[num] = i
+            print(f'key: {num}')
+            print(f'value: {hashTable[num]}\n')
+            #print(f'i: {i}\n')
+            
         
-        #iterate over the list to compute the compliment
-        for i, num in enumerate(nums):
-            compliment = target - num
+        print(f'{hashTable.keys()}')
+        print(f'{hashTable.values()}')
 
-            #check to see if compliment exits in hashmap
-            if compliment in numsDict:
-                #check to make sure we aren't using the same element twice
-                if numsDict[compliment] != i:
-                    #return the index of both numbers 
-                    return [i, numsDict[compliment]]
 
 
 #driver
